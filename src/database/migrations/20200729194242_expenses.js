74eca107febac715548b9793450c6662059f438d
dejaan <dpopovic1@etf.unsa.ba>
2020-07-29 21:40:46 +0200

    Migrations for creating tables: users, categories and expenses, another small fix

--- src/database/migrations/20200729194242_expenses.js
+++ src/database/migrations/20200729194242_expenses.js
@@ -2,8 +2,18 @@ exports.up = function (knex) {
   return knex.schema.createTable('expenses', table => {
     table.increments('id').primary();
     table.string('name').notNullable();
-    table.integer('user_id').references('users.id').notNullable();
-    table.integer('category_id').references('categories.id').notNullable();
+    table
+      .integer('user_id')
+      .references('users.id')
+      .notNullable()
+      .onDelete('CASCADE')
+      .index();
+    table
+      .integer('category_id')
+      .references('categories.id')
+      .notNullable()
+      .onDelete('CASCADE')
+      .index();
     table.float('amount').notNullable();
     table.text('description', 'LONGTEXT');
     table.timestamps(true, true);
